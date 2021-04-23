572e9c1331365df59b0cc014190fba3eabc366b1
haena3230 <gosk0912@gmail.com>
2021-04-24 01:58:11 +0900

    Add dfs

--- DFSTest.js
+++ DFSTest.js
@@ -1,3 +1,28 @@
+// 깊이 / 너비우선 탐색 네트워크 문제 --> 깊이 우선 탐색을 재귀함수를 통해 구현 
+function solution(n, computers) {
+    var answer = 0
+    let visited = new Array(n).fill(false)
+    
+    function dfs(index){
+        visited[index]=true
+        for(let i = 0 ; i < n ; i++){
+            if(i != index && visited[i] == false && computers[index][i] == 1){
+                dfs(i)
+            }
+        }
+    }
+    
+    for(let i = 0 ; i < n ; i++){
+        if(visited[i]==false){
+            answer++
+            dfs(i)
+        }
+    }
+
+    return answer
+}
+
+// 깊이/너비 우선 탐색 타겟넘버 문제 --> 깊이 우선 탐색을 stack과 queue를 통해 구현
 function solution(numbers, target) {
     var answer = 0
     let lastIndex = numbers.length-1
