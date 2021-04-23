f0a720f5e87068c007add9ad198b83585c5b7fce
haena3230 <gosk0912@gmail.com>
2021-04-24 02:41:38 +0900

    Add fullsearch

---
+++ FullSearch.js
@@ -0,0 +1,34 @@
+// 완전탐색 모의고사 문제
+function solution(answers) {
+    let length = answers.length
+    let result  = [{index : 1, count : 0}, {index: 2, count:0}, {index: 3, count:0}]
+    let one = [1,2,3,4,5]
+    let two = [2,1,2,3,2,4,2,5]
+    let three = [3,3,1,1,2,2,4,4,5,5]
+    
+    for(let i = 0 ; i < length ; i++) {
+        if(answers[i] == one[i % 5])
+            result[0].count++
+        if(answers[i] == two[i % 8])
+            result[1].count++
+        if(answers[i] == three[i % 10])
+            result[2].count++
+    }
+    
+    result.sort(function(a, b) {
+        return b.count - a.count
+    })
+    let max = result[0].count
+    let answer = [result[0].index]
+    
+    let i = 1
+    while(result[i]){
+        if(result[i].count == max){
+            answer.push(result[i].index)
+        }
+        else break
+        i++
+    }
+    
+    return answer;
+}
